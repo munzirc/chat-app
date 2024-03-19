cdbc758144bd2fcbee3d9624861f9aef311183e0
= <=>
2024-03-19 17:31:44 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,9 +10,9 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-
-
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,6 +22,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 app.get("/health", (req, res) => {
   res.send("Health Okay");
